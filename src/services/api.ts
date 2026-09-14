973273e226f10962ed92dfc30ec483d1f5ad932d
cenamoradol <isaaceverywere@gmail.com>
2026-09-13 23:00:43 -0600

    chore: point api to render backend

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // IMPORTANT: Change this to your computer's IP address to test on a physical device
-const BASE_URL = 'http://192.168.1.6:3000'; // Updated to current local IP
+const BASE_URL = 'https://walletly-backend-m94p.onrender.com';
 
 const api = axios.create({
   baseURL: BASE_URL,
